2e9152b105eb495065465fde9edf4d647df08494
Claw Kitsune <hayleytheblossomkitty@gmail.com>
2025-06-03 22:04:58 -0400

    Set up for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,21 +3,34 @@ import dotenv from "dotenv"
 import cors from "cors"
 import gamesRoutes from "./routes/gamesRoutes.js"
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173"
+        })
+    );
+}
 
 app.use(express.json());
 
-app.use(
-    cors({
-        origin: "http://localhost:5173"
-    })
-);
+
 
 app.use("/api/games", gamesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() =>
     app.listen(5001, (req, res) => {
